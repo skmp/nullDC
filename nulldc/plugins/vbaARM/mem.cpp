b7e0e2290e18d7a786846c35f4f11d5b8cced424
drkIIRaziel <drkIIRaziel@982dbc68-9c94-0dc9-9c4e-6a7e43fe6087>
2010-05-12 22:00:43 +0000

    --vbaArm: Typo when i simplified the memmap code: the api everywhere uses 1/2/4 as numeric identifiers for access sizes, i was mistakenly using 8/16/32. Fixes Issue 12 (broken sound on .. pretty much everywere ~).
    --damn, a project gets to new lows when the main bugs are typos, huh ?--

--- nulldc/plugins/vbaARM/mem.cpp
+++ nulldc/plugins/vbaARM/mem.cpp
@@ -35,10 +35,6 @@ void e68k_AcceptInterrupt()
 	update_e68k();
 }
 
-//00000000Å007FFFFF @DRAM_AREA* 
-//00800000Å008027FF @CHANNEL_DATA 
-//00802800Å00802FFF @COMMON_DATA 
-//00803000Å00807FFF @DSP_DATA 
 //Reg reads from arm side ..
 template <u32 sz,class T>
 inline T fastcall arm_ReadReg(u32 addr)
@@ -70,38 +66,20 @@ inline void fastcall arm_WriteReg(u32 addr,T data)
 void init_mem()
 {
 	aica_ram=arm_params.aica_ram;
-
-	//00000000Å 007FFFFF @DRAM_AREA* 
-	//00800000Å 008027FF @CHANNEL_DATA 
-	//00802800Å 00802FFF @COMMON_DATA 
-	//00803000Å 00807FFF @DSP_DATA 
-	/*
-	u32 total_map=0x800000;//8 mb
-	while(total_map)
-	{
-		total_map-=AICA_RAM_SIZE;
-		_vmem_map_block(aica_ram,(total_map)>>16,(total_map+AICA_RAM_MASK)>>16);
-		printf("Mapped 0x%08X to 0x%08X to ram \n",total_map,total_map+AICA_RAM_MASK);
-	}
-	_vmem_handler reg_read= _vmem_register_handler_Template(arm_ReadReg,arm_WriteReg);
-	_vmem_map_handler(reg_read,0x0080,0x0081);
-	printf("Mapped 0x%08X to 0x%08X to register funcions \n",0x00800000,0x00810000);
-
-	
-	for (u32 i=1;i<0x100;i++)
-	{
-		_vmem_mirror_mapping((i<<24)>>16,0,(1<<24)>>16);
-	}*/
 }
 //kill mem map & free used mem ;)
 void term_mem()
 {
-	//_vmem_term();
+	
 }
 
+//00000000Å 007FFFFF @DRAM_AREA* 
+//00800000Å 008027FF @CHANNEL_DATA 
+//00802800Å 00802FFF @COMMON_DATA 
+//00803000Å 00807FFF @DSP_DATA 
 
 template<int sz,typename T>
-u32 ReadMemArm(u32 addr)
+T fastcall ReadMemArm(u32 addr)
 {
 	addr&=0x00FFFFFF;
 	if (addr<0x800000)
@@ -110,13 +88,12 @@ u32 ReadMemArm(u32 addr)
 	}
 	else
 	{
-		return arm_ReadReg<sz,u32>(addr);
+		return arm_ReadReg<sz,T>(addr);
 	}
 }
 
-
 template<int sz,typename T>
-void WriteMemArm(u32 addr,T data)
+void fastcall WriteMemArm(u32 addr,T data)
 {
 	addr&=0x00FFFFFF;
 	if (addr<0x800000)
@@ -129,10 +106,10 @@ void WriteMemArm(u32 addr,T data)
 	}
 }
 
-template u32 ReadMemArm<8,u8>(u32 adr);
-template u32 ReadMemArm<16,u16>(u32 adr);
-template u32 ReadMemArm<32,u32>(u32 adr);
+template u8 ReadMemArm<1,u8>(u32 adr);
+template u16 ReadMemArm<2,u16>(u32 adr);
+template u32 ReadMemArm<4,u32>(u32 adr);
 
-template void WriteMemArm<8>(u32 adr,u8 data);
-template void WriteMemArm<16>(u32 adr,u16 data);
-template void WriteMemArm<32>(u32 adr,u32 data);
+template void WriteMemArm<1>(u32 adr,u8 data);
+template void WriteMemArm<2>(u32 adr,u16 data);
+template void WriteMemArm<4>(u32 adr,u32 data);
--- nulldc/plugins/vbaARM/mem.h
+++ nulldc/plugins/vbaARM/mem.h
@@ -2,22 +2,20 @@
 #include "aica.h"
 
 template<int sz,typename T>
-u32 ReadMemArm(u32 addr);
+T fastcall ReadMemArm(u32 addr);
 template<int sz,typename T>
-void WriteMemArm(u32 addr,T data);
+void fastcall WriteMemArm(u32 addr,T data);
 
-#define arm_ReadMem8 ReadMemArm<8,u8>
-#define arm_ReadMem16 ReadMemArm<16,u16>
-#define arm_ReadMem32 ReadMemArm<32,u32>
+#define arm_ReadMem8 ReadMemArm<1,u8>
+#define arm_ReadMem16 ReadMemArm<2,u16>
+#define arm_ReadMem32 ReadMemArm<4,u32>
 
-#define arm_WriteMem8 WriteMemArm<8,u8>
-#define arm_WriteMem16 WriteMemArm<16,u16>
-#define arm_WriteMem32 WriteMemArm<32,u32>
+#define arm_WriteMem8 WriteMemArm<1,u8>
+#define arm_WriteMem16 WriteMemArm<2,u16>
+#define arm_WriteMem32 WriteMemArm<4,u32>
 
 u32 FASTCALL sh4_ReadMem_reg(u32 addr,u32 size);
 void FASTCALL sh4_WriteMem_reg(u32 addr,u32 data,u32 size);
-//u32 sh4_ReadMem_ram(u32 addr,u32 size);
-//void sh4_WriteMem_ram(u32 addr,u32 data,u32 size);
 
 void init_mem();
 void term_mem();
