f0233525c5d5c82c0b2445c0f3fce7da42e9f109
mudlord@mudlord.info <mudlord@mudlord.info@982dbc68-9c94-0dc9-9c4e-6a7e43fe6087>
2010-09-08 09:58:19 +0000

    Added and cleaned up helper classes for mudpvr and drkpvr

--- nulldc/plugins/mudPvr/src/pvr_renderer_d3d1x.cpp
+++ nulldc/plugins/mudPvr/src/pvr_renderer_d3d1x.cpp
@@ -1,4 +1,10 @@
 #include "pvr_renderer_d3d1x.h"
+#include "pvr_impl.h"
+#include "ta.h"
+#include "regs.h"
+#include "TexCache.h"
+
+using namespace TASplitter;
 
 bool rend_init()
 {
@@ -15,6 +21,7 @@ void rend_reset(bool Manual)
 }
 bool rend_thread_start()
 {
+
 	return true;
 
 }
@@ -36,23 +43,29 @@ void rend_end_render   ()
 }
 void rend_handle_event (u32 evid,void* p)
 {
-
+	//gui resized
 }
 
 void rend_list_cont()
 {
+	//tile list continue
 
 }
 void rend_list_init()
 {
+	//tile list init
 
 }
 void rend_list_softreset()
 {
+	//tile list softreset
 
 }
 
-void rend_text_invl (vram_block* bl){}
+void rend_text_invl (vram_block* bl){
+
+	params.vram_unlock(bl);
+}
 void rend_set_render_rect (float* rect,bool  drc){}
 void rend_set_fb_scale (float x,float y){}
 void rend_list_modes(void(* callback)(u32 w,u32 h,u32 rr)){}
