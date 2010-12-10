72096b7af8e2252b1961e90ebdb4976275f51228
Mr.PsyMan <Mr.PsyMan@982dbc68-9c94-0dc9-9c4e-6a7e43fe6087>
2010-12-10 05:11:41 +0000

    clopy-paste:
    core/spu:
    spu dmas take a while to complete now. Fixes some spu-timing (?) related
    issues.
    
    *NOTE*
    We actually need a new implementation of all DMAs as well as a better timing
    system

--- trunk/nulldc/nullDC/dc/aica/aica_if.cpp
+++ trunk/nulldc/nullDC/dc/aica/aica_if.cpp
@@ -174,6 +174,27 @@ void aica_Term()
 
 }
 
+s32 aica_pending_dma = 0;
+
+void aica_periodical(u32 cycl)
+{
+	if (aica_pending_dma > 0)
+	{
+		verify(SB_ADST==1);
+
+		cycl = (aica_pending_dma <= 0) ? 0 : cycl;
+		aica_pending_dma-=cycl;
+
+		if (aica_pending_dma <= 0)
+		{
+			//log("%u %d\n",cycl,(s32)aica_pending_dma);
+			asic_RaiseInterrupt(holly_SPU_DMA);
+			aica_pending_dma = 0;
+			SB_ADST=0;
+		}
+	}
+}
+
 void Write_SB_ADST(u32 data)
 {
 	//0x005F7800	SB_ADSTAG	RW	AICA:G2-DMA G2 start address 
@@ -185,6 +206,8 @@ void Write_SB_ADST(u32 data)
 	//0x005F7818	SB_ADST	RW	AICA:G2-DMA start 
 	//0x005F781C	SB_ADSUSP	RW	AICA:G2-DMA suspend 
 	
+	u32 total_bytes=0;
+
 	if (data&1)
 	{
 		if (SB_ADEN&1)
@@ -216,11 +239,11 @@ void Write_SB_ADST(u32 data)
 
 			SB_ADSTAR+=len;
 			SB_ADSTAG+=len;
-			SB_ADST = 0x00000000;//dma done
+			total_bytes+=len;
 			SB_ADLEN = 0x00000000;
 
-			
-			asic_RaiseInterrupt(holly_SPU_DMA);
+			aica_pending_dma=((total_bytes*200000000)/65536)+1;
+			SB_ADST=1;
 		}
 	}
 }
